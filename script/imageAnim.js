19f49ad98a21eab50fa7d733cb394ad2b4a2e9dd
theMightyT <tvanrys@fanshaweonline.ca>
2019-01-28 09:08:13 -0500

    mostly working

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,112 +1,57 @@
-var animateButton, buttonHolder, beenRun, topLeft, topRight, bottomLeft, bottomRight, leftAnim, rightAnim, blAnim, brAnim,imageHolder, mainWrapper, myDelay;
+(() => {
+	// set up the puzzle pieces and boards
+	let pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-var idArray = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+	const piecesBoard = document.querySelector('.puzzle-pieces');
+	const puzzleBoard = document.querySelector('.puzzle-board');
+	const puzzleSelectors = document.querySelectorAll('#buttonHolder img');
+	const dropZones = document.querySelectorAll('.drop-zone');
 
-function init() {	
-	beenRun = false;
-	myDelay = 0;
-	animateButton = document.getElementById("playButton2");
-	buttonHolder = document.getElementById("buttonHolder");
-	mainWrapper = document.getElementById("wrapper");
-	
-	if (animateButton.addEventListener) {
-		buttonHolder.addEventListener("click", setImages);
-	} else {
-		buttonHolder.onclick = setImages;
+	function createPuzzlePieces(pictureIndex) {
+		pieces.forEach((piece, index) => {
+			// create a new puzzle piece (an image)
+			let newPuzzleImage = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="puzzle piece" draggable>`;
+			piecesBoard.innerHTML += newPuzzleImage;
+		});
+
+		// set the background image to the current puzzle
+		puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`;
+
+		initDrag();
 	}
-	
-	for (i=0; i<4; i++) {
-		console.log("now setting all of set " + i);
-		for (j=0; j<4; j++) {
-			var newPuzzleImage = document.createElement("img");
-			var idName = idArray[j] + i;
-			newPuzzleImage.id = idName;
-			newPuzzleImage.src = "images/" + idName + ".jpg";
-			newPuzzleImage.classList.add("initialScale");
-			newPuzzleImage.classList.add("makeMeAbsolute");
-			newPuzzleImage.classList.add("blur");
-			var randomX = getRandomArbitary(0, 500);
-			var randomY = getRandomArbitary(0, 300);
-			console.log("randoms: ", randomX, randomY);
-			newPuzzleImage.style.left = randomX + "px"; 
-			newPuzzleImage.style.top = randomY + "px"; 
-			newPuzzleImage.style.opacity = Math.floor((Math.random() * 100)) / 100;
-			mainWrapper.appendChild(newPuzzleImage);
-			console.log(idName);
-			console.log("opacity: ", newPuzzleImage.style.opacity); 
-		}	
+
+	function resetPuzzlePieces() {
+		piecesBoard.innerHTML = "";
+
+		createPuzzlePieces(this.dataset.puzzleindex);
 	}
-}
 
-function runAnim() {
-	beenRun = true;	
-	
-	leftAnim = TweenMax.to(topLeft, 1, {delay:myDelay, css:{ scaleX:1, scaleY:1, opacity:1, left:102, top:68, zIndex:2}});
-	leftAnim.play();
-	rightAnim = TweenMax.to(topRight, 1, {delay:myDelay, css:{scaleX:1, scaleY:1, opacity:1, left:300, top:68, zIndex:2}});
-	rightAnim.play();
-	blAnim = TweenMax.to(bottomLeft, 1, {delay:myDelay, css:{scaleX:1, scaleY:1, opacity:1, left:102, top:200, zIndex:2}});
-	blAnim.play();
-	brAnim = TweenMax.to(bottomRight, 1, {delay:myDelay, css:{scaleX:1, scaleY:1, opacity:1, left:300, top:200, zIndex:2}});
-	brAnim.play();
-	
-	myDelay = 1;
-}
-
-function setImages(e) {
-	if (beenRun) {
-		resetAnim();
+	// handle drag and drop
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img => {
+			img.addEventListener('dragstart', function(e) {
+				console.log('draggin...');
+				e.dataTransfer.setData("text/plain", this.id);
+			});
+		});
 	}
-	
-	topLeft = document.getElementById("topLeft" + e.target.id);
-	topRight = document.getElementById("topRight" + e.target.id);
-	bottomLeft = document.getElementById("bottomLeft" + e.target.id);
-	bottomRight = document.getElementById("bottomRight" + e.target.id);
-	
-	toggleBlur();
-	
-	runAnim();
-	
-	setBackground(e.target.id);
-}
 
-function resetAnim() {
-			console.log("resetAnim fired");
-			toggleBlur();
-			leftAnim.reverse();
-			rightAnim.reverse();
-			blAnim.reverse();
-			brAnim.reverse();
-			beenRun = false;
-		}
+	puzzleSelectors.forEach(button => button.addEventListener("click", resetPuzzlePieces));
 
-function toggleBlur() {
-	for (i=0; i<idArray.length; i++) {
-		(window[idArray[i]]).classList.toggle("blur");
-	}
-	/*topLeft.classList.toggle("blur");
-	topRight.classList.toggle("blur");
-	bottomLeft.classList.toggle("blur");
-	bottomRight.classList.toggle("blur");*/
-}
+	dropZones.forEach(zone => {
+		zone.addEventListener('dragover', function(e) {
+			e.preventDefault();
+			//e.dataTransfer.setData("text/plain", this.getAttribute('src'));
+			e.dataTransfer.dropEffect = "move";
+		});
 
-function getRandomArbitary (min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
+		zone.addEventListener('drop', function(e) {
+			e.preventDefault();
+			console.log('you dropped sumpin on me!');
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
 
-function setBackground(target) {
-	console.log("set background fired");
-	var nextBackground = document.getElementById("bgImage2");
-	nextBackground.src = "images/backGround" + target + ".jpg";
-	
-	var currentBackground = document.getElementById("bgImage");
-	TweenMax.to(currentBackground, 1, {delay:myDelay, css:{opacity:0}, onComplete:srcFlip});
-	
-	function srcFlip() {
-		console.log("srcFlip fired");
-		currentBackground.src = nextBackground.src;
-		currentBackground.style.opacity  = 1;
-	}
-}
-
-window.onload = init;
+	createPuzzlePieces(0);
+})();
